0a2ab8b028f444a98f1949012aa3af207405ef39
barunkapali0 <barunkapali0123@gmail.com>
2025-04-23 22:36:47 +0545

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,8 @@
+#this is my first program as an intern.
+#As the line above single comment but including this is multi line comment.
+a=input("What is your name.?:")
+b=int(input("Your age please:"))
+print(f"Namaste!{a}, Greetings.")
+print(f"So, you are {b} years old.")
+
+#the f string is used which shows the value of variable name.
