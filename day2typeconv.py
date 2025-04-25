a054b9784245b41ba44a093dbe8fd5fb5df7d940
barunkapali0 <barunkapali0123@gmail.com>
2025-04-25 12:16:46 +0545

    Add files via upload

---
+++ day2typeconv.py
@@ -0,0 +1,13 @@
+#type conversion from float to int.
+a=2.5
+b=3.2
+sum=int(a)+int(b)
+print(sum)
+
+#from int to string
+a=8888
+b="barun"
+a=str(a)
+print("your string",a)
+print(b)
+print(type(a))
---
+++ dayfloat2.py
@@ -0,0 +1,4 @@
+a=12
+b=50
+minus=float(b)-float(a)
+print(minus)
