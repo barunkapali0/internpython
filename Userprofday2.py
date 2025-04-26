e2e484a92a7a95602e60fbcf3efb38f0450092d9
barunkapali0 <barunkapali0123@gmail.com>
2025-04-26 15:55:02 +0545

    Add files via upload

---
+++ Userprofday2.py
@@ -0,0 +1,11 @@
+#a simple user profile.
+print("Lets start with the short detail to create profile user.")
+a=input("Enter your full name:")
+b=input("Enter your address:")
+c=int(input("Enter your age:"))
+d=float(input("Enter your 12th GPA:"))
+print("Name =",a)
+print("Address =",b)
+print("Age =",c)
+print("+2 GPA =",d)
+
